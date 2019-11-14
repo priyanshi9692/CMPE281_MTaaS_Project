0e97b0ebd67f4ac68daded0cebc00649bcb2534c
Priyanshi Jajoo <priyanshi.jajoo@sjsu.edu>
2019-11-13 23:54:20 -0800

    signin/register UI for Mobile TaaS

--- CMPE281_MobileTaaS/app.js
+++ CMPE281_MobileTaaS/app.js
@@ -3,14 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var session = require('client-sessions');
 var indexRouter = require('./routes/index');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(session({
+  cookieName: 'session',
+  secret: 'random_string_goes_here',
+  duration: 30 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000,
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- CMPE281_MobileTaaS/routes/index.js
+++ CMPE281_MobileTaaS/routes/index.js
@@ -1,29 +1,91 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
-/* GET home page. */
+
+
+/* SHOW LOGIN PAGE */
 router.get('/', function(req, res, next) {
+    res.render('index');
+});
+
+/* SHOW REGISTRATION PAGE */
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+
+/* SHOW REGISTRATION PAGE */
+router.get('/dashboard', function(req, res, next) {
+  if (req.session && req.session.user) {
+    if(req.session.user.type=="tester"){
+      return res.render('tester_home',{
+        user:req.session.user.name
+      });
+    } else if(req.session.user.type=="projectmanager") {
+      return res.render('manager_home',{
+        user:req.session.user.name
+      });
+    }
+  }
+  return res.redirect("/");
+  
+});
+
+
+/* Registration API */
+router.post('/register', function(req, res, next) {
+  console.log(req.body);
+  var client={};
+  client.usertype=req.body.usertype;
+  client.name=req.body.name;
+  client.email=req.body.email;
+  client.username=req.body.username;
+  client.password=req.body.pass;
   MongoClient.connect("mongodb://localhost:27017/mobile_taas", function(err, db) {
     if(!err) {
       console.log("We are connected");
     }
     var dbo = db.db("mobile_taas");
-    dbo.collection("project_manager").find({}).toArray(function(err, result) {
-      if (err) throw err;
-      //console.log(result);
-      var data =[];
-      for(var i=0;i<result.length;i++){
-        var manager={};
-        manager.firstname=result[i].firstname;
-        manager.lastname = result[i].lastname;
-        manager.email = result[i].email;
-        data.push(manager);
-        console.log(manager);
-      }
-    res.render('index', { title: 'Mobile-TaaS' });
+   
+    dbo.collection("client").insertOne(client, function(err, result) {
+      if(err)throw err;
+
+      
+    res.send("Successfully inserted");
       }); 
 });
-  
+});
 
-  });
+
+/* LOGIN CHECK API */
+router.get('/login', function(req, res, next) {
+  var client={};
+
+  var username=req.query.username;
+  var password=req.query.password;
+  console.log(password);
+  MongoClient.connect("mongodb://localhost:27017/mobile_taas", function(err, db) {
+    if(!err) {
+      console.log("We are connected");
+    }
+    var dbo = db.db("mobile_taas");
+    dbo.collection("client").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      for(var i=0;i<result.length;i++){
+       
+        if(username==result[i].username && password==result[i].password){
+          client.name = result[i].name;
+          client.username=result[i].username;
+          client.type=result[i].usertype;
+          console.log(client);
+          req.session.user = client;
+          return res.send("success");
+        }
+       
+      }
+      return res.send("username or password is incorrect, please try again!");
+   
+      }); 
+});
+});
 module.exports = router;
